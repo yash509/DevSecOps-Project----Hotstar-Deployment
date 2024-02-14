7ea7326310b049370986f00788e02e5aa2a4175b
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-14 19:41:56 +0530

    Update Nav.jsx

--- sources/extras/Nav.jsx
+++ sources/extras/Nav.jsx
@@ -1,1 +1,15 @@
-
+import React from 'react'
+import './Nav.css'
+function Nav() {
+  return (
+    <div className='na'>
+        <div className="icc">
+        <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-tv"></i>
+        </div>
+    </div>
+  )
+}
+export default Nav
